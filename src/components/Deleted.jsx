74c1425bd3a2df0460e7aa376963ade76e422ebd
Yeonjin Lee <89119982+thisisyjin@users.noreply.github.com>
2022-06-15 16:07:14 +0900

    add date count

--- src/components/Deleted.jsx
+++ src/components/Deleted.jsx
@@ -1,15 +1,23 @@
 import { StyledLink, DeleteBlock } from '../styles/DetailStyle';
 import { ReactComponent as Delete } from '../assets/delete.svg';
+import { useParams } from 'react-router-dom';
 
 const Deleted = () => {
+  const { id } = useParams();
+  const dateDiff = new Date() - new Date(+id);
+  const perDay = 24 * 60 * 60 * 1000;
+  const dayCount = Math.floor(dateDiff / perDay);
+
   return (
     <DeleteBlock>
       <div className="deleted-content">
         <Delete />
         <p className="deleted-text">
-          Your wish come true! <br />
+          Your wish comes true! <br />
           Keep Going!
+          <span className="date-diff">in {dayCount} days</span>
         </p>
+
         <StyledLink to="/">OK</StyledLink>
       </div>
     </DeleteBlock>
